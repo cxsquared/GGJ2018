7d67d90c9cf6e5a65ada11828de01576ca72705d
Cody Claborn <cxsquared@gmail.com>
2018-01-27 10:59:25 -0500

    Added basic path finding line rendering.

---
+++ Assets/scripts/NavigationTrail.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+[RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(LineRenderer))]
+public class NavigationTrail : MonoBehaviour {
+
+    [SerializeField]
+    private Transform[] _transistors;
+    private Transform[] Transistors{ get { return _transistors; } set { _transistors = value; } }
+
+    [SerializeField]
+    private Transform _radioTower;
+    private Transform RadioTower { get { return _radioTower; } set { _radioTower = value; } }
+
+    [SerializeField]
+    private float _trailOpacity = .5f;
+    private float TrailOpacity { get { return _trailOpacity; } set { _trailOpacity = value; } }
+
+    private NavMeshAgent Agent { get; set; }
+    private LineRenderer Line { get; set; }
+    private Transform CurrentTarget { get; set; }
+    private NavMeshPath Path { get; set; } 
+
+	// Use this for initialization
+	void Start () {
+        Agent = GetComponent<NavMeshAgent>();
+        Path = new NavMeshPath();
+        Line = GetComponent<LineRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        var oldCurrentTarget = CurrentTarget;
+        if (CurrentTarget == null)
+        {
+            CurrentTarget = RadioTower;
+        }
+
+        var distanceToTarget = Vector3.Distance(this.transform.position, CurrentTarget.position);
+
+        foreach (var transistor in Transistors)
+        {
+            var newDistance = Vector3.Distance(this.transform.position, transistor.position);
+
+            if (newDistance < distanceToTarget)
+            {
+                CurrentTarget = transistor;
+                distanceToTarget = newDistance;
+            }
+        }
+
+        if (CurrentTarget != oldCurrentTarget)
+        {
+            if (Agent.CalculatePath(CurrentTarget.position, Path))
+            {
+                Line.positionCount = Path.corners.Length;
+                for( var i = 0; i < Path.corners.Length; i++)
+                {
+                    Line.SetPosition(i, Path.corners[i]);
+                }
+            }
+        }
+	}
+}
