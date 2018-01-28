a17762dc6fb863c1b98d49b245b0d54d75ca2400
Cody Claborn <codylclaborn@gmail.com>
2018-01-28 15:11:18 -0500

    Added tower to send

--- Assets/scripts/DiscoveryZone.cs
+++ Assets/scripts/DiscoveryZone.cs
@@ -6,9 +6,15 @@ public class DiscoveryZone : MonoBehaviour {
 
 	public GameEventEnum objectType;
 
+    bool IsSent = false;
+
 	void OnTriggerEnter(Collider other) {
 		if (other.CompareTag ("Player")) {
 			DataGatherer.Instance.AddEvent (new GameEvent (objectType));
+            if (objectType == GameEventEnum.DISCOVER_RADIO_TOWER && !IsSent)
+            {
+                DataGatherer.Instance.SendTweet(1);
+            }
 		}
 	}
 
