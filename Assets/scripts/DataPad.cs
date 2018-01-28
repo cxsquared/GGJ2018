172cdce162e4b723f275e39616a714976cb0da3d
Cody Claborn <codylclaborn@gmail.com>
2018-01-28 14:48:00 -0500

    Added data pad.

---
+++ Assets/scripts/DataPad.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;   
+
+[RequireComponent(typeof(SphereCollider))]
+public class DataPad : MonoBehaviour {
+
+    bool HasTriggered = false;
+
+    bool IsVisible = false;
+
+    public Text textObj;
+    public Text instructions;
+
+    public string Entry;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (IsVisible && Input.GetKeyDown(KeyCode.E))
+        {
+            textObj.gameObject.SetActive(false);
+            instructions.gameObject.SetActive(false);
+        }	
+	}
+
+    private void  OnTriggerEnter(Collider other)
+    {
+        if (!HasTriggered && other.tag == "Player")
+        {
+            HasTriggered = true;
+            IsVisible = true;
+            textObj.text = Entry;
+            textObj.gameObject.SetActive(true);
+            instructions.gameObject.SetActive(true);
+        }
+    }
+}
