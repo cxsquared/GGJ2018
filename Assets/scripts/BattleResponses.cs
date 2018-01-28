db9123c90727c00ba079c39749647ca65a4c6df1
Faas, Travis <tfaas@iupui.edu>
2018-01-27 23:44:19 -0500

    Adding kill features for enemies

--- Assets/scripts/BattleResponses.cs
+++ Assets/scripts/BattleResponses.cs
@@ -5,17 +5,21 @@ using UnityEngine;
 public class BattleResponses : MonoBehaviour {
 
 	private Rigidbody rb;
+	private Animator animator;
 	public int hp;
 
 	// Use this for initialization
 	void Start () {
 		rb = this.GetComponent<Rigidbody> ();
+		animator = this.GetComponent<Animator> ();
 	}
 
 	public void AttackHit(object[] hitInfo) {
 		rb.AddForce (( Vector3) hitInfo [1]);
 		hp -= (int) hitInfo[0];
+
         SoundManager.Instance.PlayBigEnemyHit(transform.position);
+		animator.SetTrigger ("stagger");
 
 		if (hp <= 0) {
 			TriggerDeath ();
@@ -24,11 +28,11 @@ public class BattleResponses : MonoBehaviour {
 
 	void TriggerDeath() {
         SoundManager.Instance.PlayBigEnemyDeath(transform.position);
+		animator.SetTrigger ("kill");
+	}
+
+	public void FinishDeath() {
 		Destroy (this.gameObject);
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+
 }
